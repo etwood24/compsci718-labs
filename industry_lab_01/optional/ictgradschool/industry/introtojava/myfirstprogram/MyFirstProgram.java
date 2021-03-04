e0e41537dbc2897902062652c5cc0c734278897c
Evan Wood <ewoo030@aucklanduni.ac.nz>
2021-03-04 17:59:57 +1300

    optional ex

--- industry_lab_01/optional/ictgradschool/industry/introtojava/myfirstprogram/MyFirstProgram.java
+++ industry_lab_01/optional/ictgradschool/industry/introtojava/myfirstprogram/MyFirstProgram.java
@@ -1,12 +1,12 @@
 package ictgradschool.industry.introtojava.myfirstprogram;
 
 public class MyFirstProgram {
-    public void start() {
-        System.out.println("Hello World");
+    public void start(String name) {
+        System.out.println("Hello " + name);
     }
     
     public static void main(String[] args) {
         MyFirstProgram p = new MyFirstProgram();
-        p.start();
+        p.start(args[0]);
     }
 }
